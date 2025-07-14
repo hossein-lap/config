cda8293fdfcc075b892749756c3fcb8fabb5dbde
Hossein Esmail <hosteam01@gmail.com>
2025-07-15 00:42:28 +0330

    removed yarn :/

--- yarn/global/node_modules/pyright/dist/typeshed-fallback/stubs/pywin32/win32/lib/win2kras.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-def __getattr__(name: str) -> Any: ...  # incomplete
-
-RASEAPF_Logon: int
-RASEAPF_NonInteractive: int
-RASEAPF_Preview: int
-
-def GetEapUserIdentity(*args, **kwargs): ...  # incomplete
